7a1d40868b854736a00c257904eda2ef2790514f
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 23:49:52 -0800

    Project Files

---
+++ homeWorkWeek2Problem5 method w_American format/homeWorkWeek2Problem5 method w_American format/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkWeek2Problem5_method_w_American_format
+{
+    class Program
+    {
+        static void Main(string[] args)                                                  // Evan Caraway, Srinivas Reddy Doma, Kipack Jeong
+        {
+            Console.Write("Enter day: ");
+            string day = Console.ReadLine();
+            Console.Write("Enter date: ");
+            int date = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter month: ");
+            string month = Console.ReadLine();
+            Console.Write("Enter year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            WriteAmerican(day, month, date, year);                                      // Call method WriteAmerican in main.
+        }
+        static void WriteAmerican(string day, string month, int date, int year)         // Method that prints day, date, month and year in American format.
+        {
+            Console.WriteLine($"The current date is {day}, {month} {date}, {year}.");
+        }
+    }
+}
---
+++ homeWorkWeek2Problem5 method w_American format/homeWorkWeek2Problem5 method w_American format/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("homeWorkWeek2Problem5 method w_American format")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("homeWorkWeek2Problem5 method w_American format")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f08361c-d37d-43d4-88bc-92e6ba2c9ae5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
